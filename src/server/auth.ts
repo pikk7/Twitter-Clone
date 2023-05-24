21a71d2c96cf5deafad04ab65ea21665d17eb564
pikk7 <durza13@gmail.com>
2023-05-24 19:49:31 +0200

    GithubProvider delete

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -8,7 +8,6 @@ import DiscordProvider from "next-auth/providers/discord";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { env } from "~/env.mjs";
 import { prisma } from "~/server/db";
-import GitHubProvider from "next-auth/providers/github";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
